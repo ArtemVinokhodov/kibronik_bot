8dd63271bd201ad39d748821afe1176fd388cb1f
Artem Vinokhodov <a.vinohod@gmail.com>
2025-03-31 09:30:40 +0200

    fix: запускаем start_bot без потока

--- web.py
+++ web.py
@@ -1,7 +1,6 @@
-import asyncio
 from flask import Flask
-from threading import Thread
 from bot import start_bot
+from threading import Thread
 
 app = Flask(__name__)
 
@@ -12,11 +11,7 @@ def index():
 def run_flask():
     app.run(host='0.0.0.0', port=10000)
 
-def run_bot():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(start_bot())
-
 if __name__ == '__main__':
     Thread(target=run_flask).start()
-    Thread(target=run_bot).start()
+    # ❗ Вызываем без потока, просто запускаем как основной процесс
+    start_bot()
