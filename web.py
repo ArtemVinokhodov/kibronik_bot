85bf046f194ea2b424a723a29f4d41de891c6cb8
Artem Vinokhodov <a.vinohod@gmail.com>
2025-03-31 09:24:48 +0200

    Fix: event loop for start_bot() in separate thread

--- web.py
+++ web.py
@@ -1,3 +1,4 @@
+import asyncio
 from flask import Flask
 from threading import Thread
 from bot import start_bot
@@ -11,6 +12,11 @@ def index():
 def run_flask():
     app.run(host='0.0.0.0', port=10000)
 
+def run_bot():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(start_bot())
+
 if __name__ == '__main__':
     Thread(target=run_flask).start()
-    Thread(target=start_bot).start()
+    Thread(target=run_bot).start()
